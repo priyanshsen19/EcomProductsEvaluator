3a456095ab7be4f6b3d1bfe99d92976109fd8e89
Priyansh S <priyansh.s@caratlane.com>
2025-10-10 01:04:08 +0530

    dotenv to prevent overriding

--- services/product-service/src/server.ts
+++ services/product-service/src/server.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ override: false });
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
